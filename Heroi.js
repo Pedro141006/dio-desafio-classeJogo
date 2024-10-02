fddc83e38dd4bbe9d37d0492f75574ab6c41ff4a
Pedro Henrique <phpdef@gmail.com>
2024-10-01 22:45:48 -0400

    new

--- Heroi.js
+++ Heroi.js
@@ -1,3 +1,28 @@
 class Heroi {
-  constructor(nome, idade, tipo) {}
+  constructor(nome, idade, tipo) {
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo
+  }
+
+  atacar() {
+    let classe = this.tipo
+    let ataque
+
+    if (classe === 'mago') {
+      ataque = 'magia'
+    } else if (classe === 'guerreiro') {
+      ataque = 'espada'
+    } else if (classe === 'monge') {
+      ataque = 'artes marciais'
+    } else if (classe === 'ninja') {
+      ataque = 'shuriken'
+    }
+
+    console.log(`o ${classe} atacou usando ${ataque}`)
+  }
 }
+
+const pilarDaPedra = new Heroi('Gyomei Himejima', 27, 'monge')
+
+pilarDaPedra.atacar()
